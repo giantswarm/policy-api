6c9ac727048ddcc3cae15cd59d4db87a022f93cc
Franco <franco@giantswarm.io>
2024-04-09 19:32:07 -0300

    Initialize git repo

---
+++ api/v1alpha1/policyexception_types.go
@@ -0,0 +1,63 @@
+/*
+Copyright 2024.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package v1alpha1
+
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+//+kubebuilder:resource:shortName=gspolex
+//+kubebuilder:subresource:status
+
+// PolicyExceptionSpec defines the desired state of PolicyException
+type PolicyExceptionSpec struct {
+	// Policies defines the list of policies to be excluded
+	Policies []string `json:"policies"`
+
+	// Targes defines the list of target workloads where the exceptions will be applied
+	Targets []Target `json:"targets"`
+}
+
+//+kubebuilder:object:root=true
+
+// PolicyException is the Schema for the policyexceptions API
+type PolicyException struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec PolicyExceptionSpec `json:"spec,omitempty"`
+}
+
+// Target defines a resource to which a PolicyException applies
+type Target struct {
+	Namespaces []string `json:"namespaces"`
+	Names      []string `json:"names"`
+	Kind       string   `json:"kind"`
+}
+
+//+kubebuilder:object:root=true
+
+// PolicyExceptionList contains a list of PolicyException
+type PolicyExceptionList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+	Items           []PolicyException `json:"items"`
+}
+
+func init() {
+	SchemeBuilder.Register(&PolicyException{}, &PolicyExceptionList{})
+}
---
+++ api/v1alpha1/policymanifest_types.go
@@ -0,0 +1,54 @@
+/*
+Copyright 2024.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package v1alpha1
+
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// PolicyManifestSpec defines the desired state of PolicyManifest
+type PolicyManifestSpec struct {
+	// Foo is an example field of PolicyManifest. Edit policymanifest_types.go to remove/update
+	Mode                string   `json:"mode"`
+	Args                []string `json:"args"`
+	Exceptions          []Target `json:"exceptions"`
+	AutomatedExceptions []Target `json:"automatedExceptions"`
+}
+
+//+kubebuilder:object:root=true
+//+kubebuilder:subresource:status
+
+// PolicyManifest is the Schema for the policymanifests API
+type PolicyManifest struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec PolicyManifestSpec `json:"spec,omitempty"`
+}
+
+//+kubebuilder:object:root=true
+
+// PolicyManifestList contains a list of PolicyManifest
+type PolicyManifestList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+	Items           []PolicyManifest `json:"items"`
+}
+
+func init() {
+	SchemeBuilder.Register(&PolicyManifest{}, &PolicyManifestList{})
+}
