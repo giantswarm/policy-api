fa72c8e41c05331473aedeb5b32eadc1d141e7e5
Franco <franco@giantswarm.io>
2024-04-17 17:18:32 -0300

    Update tools and run

--- hack/generate-code.sh
+++ hack/generate-code.sh
@@ -6,7 +6,10 @@ set -o pipefail
 
 readonly SCRIPT_ROOT="$(cd "$(dirname "${BASH_SOURCE}")"/.. && pwd)"
 
+readonly OUTPUT_PKG=github.com/giantswarm/policy-api/pkg/client
 readonly APIS_PKG=github.com/giantswarm/policy-api
+readonly CLIENTSET_NAME=versioned
+readonly CLIENTSET_PKG_NAME=clientset
 readonly VERSIONS=(v1alpha1)
 
 GATEWAY_INPUT_DIRS=""
@@ -37,5 +40,36 @@ go run k8s.io/code-generator/cmd/applyconfiguration-gen \
     --openapi-schema <(go run "${SCRIPT_ROOT}/cmd/modelschema") \
     --output-package "${APIS_PKG}/api/applyconfiguration" \
     ${COMMON_FLAGS}
+
+echo "Generating clientset at ${OUTPUT_PKG}/${CLIENTSET_PKG_NAME}"
+go run k8s.io/code-generator/cmd/client-gen \
+  --clientset-name "${CLIENTSET_NAME}" \
+  --input-base "${APIS_PKG}" \
+  --input "${GATEWAY_INPUT_DIRS//${APIS_PKG}/}" \
+  --output-package "${OUTPUT_PKG}/${CLIENTSET_PKG_NAME}" \
+  --apply-configuration-package "${APIS_PKG}/api/applyconfiguration" \
+  ${COMMON_FLAGS}
+
+echo "Generating informers at ${OUTPUT_PKG}/informers"
+go run k8s.io/code-generator/cmd/informer-gen \
+  --input-dirs "${GATEWAY_INPUT_DIRS}" \
+  --versioned-clientset-package "${OUTPUT_PKG}/${CLIENTSET_PKG_NAME}/${CLIENTSET_NAME}" \
+  --listers-package "${OUTPUT_PKG}/listers" \
+  --output-package "${OUTPUT_PKG}/informers" \
+  ${COMMON_FLAGS}
+
+# echo "Generating ${VERSION} register at ${APIS_PKG}/api/${VERSION}"
+# go run k8s.io/code-generator/cmd/register-gen \
+#   --input-dirs "${GATEWAY_INPUT_DIRS}" \
+#   --output-package "${APIS_PKG}/api" \
+#   ${COMMON_FLAGS}
+
+for VERSION in "${VERSIONS[@]}"
+do
+  echo "Generating ${VERSION} deepcopy at ${APIS_PKG}/api/${VERSION}"
+  go run sigs.k8s.io/controller-tools/cmd/controller-gen \
+    object:headerFile=${SCRIPT_ROOT}/hack/boilerplate.go.txt \
+    paths="${APIS_PKG}/api/${VERSION}"
+done
 
 rm -f "$GOPATH/src/github.com/giantswarm/policy-api"
